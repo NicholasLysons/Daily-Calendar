6343064f255118ca9e8421fc90ee60e212fd207f
NicholasLysons <boylysons@icloud.com>
2023-05-01 21:46:23 -0700

    touched up styling

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
-function displayTime(){
+$(function () {
+    function displayTime(){
  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a')
  timeDisplayEl.text(rightNow);
-}
-displayTime();
-setInterval(displayTime, 1000);
+    }
+    displayTime();
+    setInterval(displayTime, 1000);
+});
