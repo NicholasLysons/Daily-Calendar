2f6d5398fcca29f0476652ebb381c62b1a611d2c
NicholasLysons <boylysons@icloud.com>
2023-05-16 14:38:32 -0700

    added to readme and script

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,74 @@
 var timeDisplayEl = $('#time-display');
 
 
+function displayTime() 
+    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
 
-$(function () {
-}); 
-function displayTime()
- var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
- timeDisplayEl.text(rightNow);
+function readEventFromStorage() {
+    var event = localStorage.getItem('event');
+    if (event) {
+      event = JSON.parse(event);
+    } else {
+      event = [];
+    }
+    return event;
+}
+
+function saveEventToStorage(event) {
+    localStorage.setItem('event', JSON.stringify(event));
+}
+
+function printEventData() {
+
+    eventDisplayEl.empty();
+  
+    var event = readEventFromStorage();
+
+    rowEl.append(nameEL, typeEl, dateEl, deleteEl);
+    eventDisplayEl.append(rowEl);
+}
+  
+
+function handleDeleteEvent() {
+    var eventIndex = parseInt($(this).attr('data-index'));
+    var event = readEventFromStorage();
+
+    event.splice(eventIndex, 1);
+    saveEventToStorage(event);
+  
+    printEventData();
+}
+
+function handleEventFormSubmit(event) {
+    event.preventDefault();
+
+  var eventName = eventNameInputEl.val().trim();
+  var eventType = eventTypeInputEl.val(); 
+  var eventDate = eventDateInputEl.val();
+
+    var newEvent = {
+        name: eventName,
+        type: eventType,
+        date: eventDate,
+    };
+
+
+  var event = readEventFromStorage();
+  event.push(newEvent);
+  saveEventToStorage(event);
+
+  printEventData();
+
+  eventNameInputEl.val('');
+  eventTypeInputEl.val('');
+  eventDateInputEl.val('');
+}
+
+eventFormEl.on('submit', handleEventFormSubmit);
+
+layEl.on('click', '.btn-delete-event', handleDeleteEvent);
+
 
 displayTime();
 setInterval(displayTime, 1000);
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Daily-Calendar
+## Daily-Calendar
 
-A calendar API to plan out your daily work day
+
+Have you ever wanted an app that can allow you to plan out your work day each day while at home or on the go? Well with this daily planner you can have the ability to `plan your` day by typing in a time that you are busy, then it will be blocked out while the rest are working.
+
+## Screenshot
+
+## Author
+
+
+
+
